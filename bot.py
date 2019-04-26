365fd558fce358858837eb353e84e75bd2e5eadd
darkkeks <darkkeks@rambler.ru>
2019-04-26 23:48:44 +0300

    Added link generation

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import logging
 import os
 
+import requests
 from vk_api import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
@@ -19,6 +20,8 @@ logger.addHandler(console_handler)
 logger.setLevel(logging.INFO)
 
 
+merchant_url = 'https://www.digiseller.market/asp2/pay_wm.asp?id_d=2629111&lang=ru-RU&referrer=bank&vk_id={id}'
+
 group_id = os.environ.get('GROUP_ID')
 group_token = os.environ.get('GROUP_TOKEN')
 
@@ -31,20 +34,20 @@ class Messages:
 
 📋 Цены - узнать текущий курс продажи VK Coin
 
-📝 Инструкция — получить инструкцию по покупке VK Coin"""
+📝 Инструкция — получить инструкцию по покупке VK Coin
+"""
 
     Intro = """✌️ Привет!
 Этот бот позволяет купить VK Coin по выгодному курсу‼️
 Если сомневаешься в нашей честности, можешь прочитать отзывы наших покупателей: https://vk.cc/9gFQYk
-
 """
 
-    BuyGuide = """📝 Инструкция по покупке VK Coin у бота: vk.cc/9giWdY"""
-    Buy = """💰 Оформление заказа и оплата: vk.cc/9gj1GK
+    BuyGuide = """📝 Инструкция по покупке VK Coin у бота: https://vk.cc/9giWdY"""
+
+    Buy = """💰 Оформление заказа и оплата: {url}
     
-Отзывы покупателей: https://vk.cc/9gFQYk    
-    
-После оплаты отправьте сюда уникальный код, выданный Вам после оплаты!"""
+Отзывы покупателей: https://vk.cc/9gFQYk
+"""
 
     Rate = """Текущий курс (бот только продает коины, не покупает): 
     
@@ -52,8 +55,6 @@ class Messages:
 💰 500 000 VK Coin - 45 рублей, 
 💰 1 000 000 VK Coin - 90 рублей, 
 💰 2 000 000 VK Coin - 162 рублей (скидка 10%‼️)"""
-
-    Bonus = """Инструкция о том, как получить VK Coin бесплатно: vk.cc/9giVX9"""
 
 
 class Bot:
@@ -71,8 +72,6 @@ class Bot:
         add_button('Инструкция')
         self.main_keyboard.add_line()
         add_button('Цены', color=VkKeyboardColor.DEFAULT)
-        self.main_keyboard.add_line()
-        add_button('Получить VK Coins бесплатно!', color=VkKeyboardColor.NEGATIVE)
 
     def start(self):
         for event in self.bot.listen():
@@ -86,13 +85,11 @@ class Bot:
                     if message == 'начать':
                         self.send_message(id, Messages.Intro + Messages.Commands)
                     elif message == 'купить':
-                        self.send_message(id, Messages.Buy)
+                        self.send_message(id, Messages.Buy.format(url=self.get_url(id)))
                     elif message == 'инструкция':
                         self.send_message(id, Messages.BuyGuide)
                     elif message == 'цены':
                         self.send_message(id, Messages.Rate)
-                    elif message == 'Получить VK Coins бесплатно!'.lower():
-                        self.send_message(id, Messages.Bonus)
                     else:
                         self.send_message(id, Messages.Commands)
                 else:
@@ -105,6 +102,11 @@ class Bot:
             keyboard=self.main_keyboard.get_keyboard(),
             message=message
         )
+
+    @staticmethod
+    def get_url(id):
+        url = merchant_url.format(id=id)
+        return requests.get('https://clck.ru/--', params={'url': url}).text
 
     @staticmethod
     def has_market_attachment(message):
